73a96126b4cf6c5ce0dbe8d1f513578a6a0311bd
AG0811 <ag114126+d@gmail.com>
2024-05-24 16:09:48 +0900

    Rubocopを実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
 
   private
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       # 指定されている要素のみでログインできる記述
       # username == 'admin!' && password == '2222'
       # 環境変数を読み込む記述
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
